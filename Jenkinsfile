c6ec31ca125ccfeceb7c77aea12c97993f617141
alejandromunicio <amunicio@DESKTOP-E4T88KI.localdomain>
2019-10-18 12:40:42 +0200

    Another change

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,6 +8,7 @@ pipeline {
     environment {
         CI = 'true'
 	USERNAME = credentials('USERNAME')
+	MY_PASSWORD = credentials('MY_PASSWORD')
     }
     stages {
         stage('Build') {
--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/notok', (req, res) => res.json({ ok: false }))
 
 app.get('/username', (req, res) => res.json({ username: process.env.USERNAME }))
 
+app.get('/usernamePassword', (req, res) => res.json({
+	username: process.env.MY_PASSWORD_USR
+	password: process.env.MY_PASSWORD_PSW
+}))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = { app };
